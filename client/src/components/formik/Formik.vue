8668e03ccd53b79c143e63f7a39aab5b5199cdae
Kevin Taing <taingkn@gmail.com>
2020-01-07 12:45:17 +0100

    wip

---
+++ client/src/components/formik/Formik.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "formik",
+  props: {
+    initialValues: Object
+  },
+  data: function() {
+    return {
+      values: {}
+    };
+  },
+  methods: {
+    setFormValue: function(key, value) {
+      this.form.values[key] = value;
+    },
+    getFormInitialValue: function(key) {
+      return this.initialValues[key];
+    },
+    handleSubmit: function(event) {
+      this.$emit("submit", {
+        event,
+        values: this.values
+      });
+    }
+  },
+  provide: function() {
+    return {
+      setFormValue: this.setFormValue,
+      getFormInitialValue: this.getFormInitialValue
+    };
+  }
+};
+</script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,46 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import "./registerServiceWorker";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+const requireComponent = require.context(
+  // The relative path of the components folder
+  "./components/formik",
+  // Whether or not to look in subfolders
+  false,
+  // The regular expression used to match form component filenames
+  /Form[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+  // Get component config
+  const componentConfig = requireComponent(fileName);
+  // console.log('fileName',fileName
+  //   .split('/')
+  //   .pop()
+  //   .replace(/\.\w+$/, ''))
+
+  // Get PascalCase name of component
+  const componentName = fileName
+    .split("/")
+    .pop()
+    .replace(/\.\w+$/, "");
+
+  // Register component globally
+  Vue.component(
+    componentName,
+    // Look for the component options on `.default`, which will
+    // exist if the component was exported with `export default`,
+    // otherwise fall back to module's root.
+    componentConfig.default || componentConfig
+  );
+});
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
