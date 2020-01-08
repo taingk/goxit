5667a21b07c8dff83b8ff763490af4997a7deb7b
Kevin Taing <taingkn@gmail.com>
2020-01-08 11:28:23 +0100

    fix formik

--- client/src/components/formik/Field.vue
+++ client/src/components/formik/Field.vue
@@ -19,7 +19,7 @@ export default {
   },
   data() {
     return {
-      input: 'input'
+      input: 'Input'
     };
   }
 };
--- client/src/main.js
+++ client/src/main.js
@@ -5,7 +5,7 @@ import store from './store';
 import './registerServiceWorker';
 import axios from 'axios';
 
-Vue.use(axios);
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false;
 
 new Vue({
