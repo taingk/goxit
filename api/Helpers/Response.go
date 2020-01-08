c8ac463d6db8fb85db1c16fa783d39d1942f4c94
Kevin Taing <taingkn@gmail.com>
2020-01-08 12:29:46 +0100

    simplify api response

--- api/Helpers/Response.go
+++ api/Helpers/Response.go
@@ -6,19 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type ResponseData struct {
-	Status int
-	Meta   interface{}
-	Data   interface{}
-}
-
 func RespondJSON(w *gin.Context, status int, payload interface{}) {
-	fmt.Println("status ", status)
-	var res ResponseData
-
-	res.Status = status
-
-	res.Data = payload
-
-	w.JSON(200, res)
+	w.JSON(status, payload)
 }
